b50dfc9c818c4e862667d27a4ae90ef18ff37fa7
stephane-perez <stephane-perez@users.noreply.github.com>
2026-08-26 14:28:06 +0200

    Useless call of 'weight'

--- app/src/main/java/com/stephaneperez/notepad/ui/NotepadScreen.kt
+++ app/src/main/java/com/stephaneperez/notepad/ui/NotepadScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
